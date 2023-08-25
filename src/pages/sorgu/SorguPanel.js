888a17433c8e3108c68538e02c8575dd710f8270
outputtxt <142933373+outputtxt@users.noreply.github.com>
2023-08-25 11:04:56 +0000

    Update 4 files

--- src/pages/sorgu/SorguPanel.js
+++ src/pages/sorgu/SorguPanel.js
@@ -1,34 +1,51 @@
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
+import { useState } from "react";
 import SorguMenu from "./menu/SorguMenu";
 import "./sorguPanel.css";
 
-export default function SorguPanel() {
+const SorguPanel = () => {
+  const [open, setOpen] = useState(true);
+  const openWidth = "300px";
+  const closedWidth = "25px";
+
+  const toggleOpen = () => {
+    setOpen(!open);
+  };
+
   return (
     <div
       id="container"
-      style={{
-        display: "flex",
-        margin: 0,
-        padding: 0,
-      }}
+      style={{ borderLeft: "1px solid white", borderright: "1px solid white" }}
     >
-      <div
-        id="left"
-        style={{
-          display: "flex",
-        }}
-      >
-        <SorguMenu />
+      <div id="top" style={{ width: open ? openWidth : closedWidth }}>
+        <table width={"100%"}>
+          <thead>
+            <tr>
+              {open && (
+                <th width="90%" style={{ width: "90%", paddingLeft: "60px" }}>
+                  Sorgu Panel
+                </th>
+              )}
+
+              <th width="10%">
+                {" "}
+                <button onClick={toggleOpen} className="menuBtn">
+                  {open ? (
+                    <KeyboardDoubleArrowLeftIcon />
+                  ) : (
+                    <KeyboardDoubleArrowRightIcon />
+                  )}
+                </button>
+              </th>
+            </tr>
+          </thead>
+        </table>
       </div>
-      <div id="right">
-        <div id="top">
-          <h2>Sorgu Panel BAR</h2>
-        </div>
-        <div id="bottom">
-          <br />
-          <br />
-          <br /> content
-        </div>
-      </div>
+
+      <div>{open ? <SorguMenu /> : <div id="bottom" />}</div>
     </div>
   );
-}
+};
+
+export default SorguPanel;

8bc2541ee90d7e3399414236d1b14d6575d9e88f
outputtxt <142933373+outputtxt@users.noreply.github.com>
2023-09-08 15:23:19 +0000

    Update 6 files

--- src/pages/sorgu/sol/koordinat/KoordinatSorguPanel.js
+++ src/pages/sorgu/sol/koordinat/KoordinatSorguPanel.js
@@ -1,49 +1,66 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { useForm } from "react-hook-form";
 import Box from "@mui/material/Box";
+import { MapContext } from "../../../../util/context/Context";
+import { LATITUDE_REGEX, LONGITUDE_REGEX } from "../../../../util/Constants";
 
 const KoordinatSorguPanel = () => {
+    const { register, handleSubmit, reset, formState: { errors } } = useForm({mode: "onChange"});
     const [x, setX] = useState(null);
     const [y, setY] = useState(null);
 
-    const handleKoordinatSubmit = (event) => {
-        event.preventDefault();
-        alert("Koordinat: ["+ x + ", " + y + "]");
-        const enlem = document.getElementById("enlem");
-        enlem.classList.add("invalid");
-        return false;
+    const { map } = useContext(MapContext);
+
+    const handleKoordinatSubmit = () => {
+        map.setView([x, y], map.getZoom());
     }
 
     const onXChange = (event) => {
         setX(event.target.value);
-        event.preventDefault();
     }
 
     const onYChange = (event) => {
         setY(event.target.value);
-        event.preventDefault();
     }
 
     return (
         <Box component="fieldset" className="sorgu-fieldset">
             <legend className="sorgu-fieldset-legend">Koordinatlar</legend>
-            <form onSubmit={event => handleKoordinatSubmit(event)} style={{lineHeight: "30px"}}>
-                <label className="sorgu-label" htmlFor="enlem">Enlem (X) </label>
-                <input style={{ width:"150px" }} onChange={event => onXChange(event)} id="enlem" required 
-                    onInvalid={e => e.target.setCustomValidity("Lütfen Enlem Giriniz !")}
-                    onInput={e => e.target.setCustomValidity('')} 
-                    name="X"
-                    pattern="/^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/"
+
+            <form onSubmit={handleSubmit(handleKoordinatSubmit)} style={{lineHeight: "30px"}}>
+                <div>
+                    <label className="sorgu-label" htmlFor="enlemX">Enlem &nbsp;(X) </label>
+                    <input className={errors.enlemX ? 'sorgu-form-data-error' : 'sorgu-form-data-normal'} 
+                        value={undefined}
+                        type="text"
+                        id="enlemX"
+                        {...register("enlemX", {
+                            onChange: event => onXChange(event),
+                            required: true,
+                            pattern: LATITUDE_REGEX
+                        })}
                     />
-                <br/>
-                <label className="sorgu-label" htmlFor="boylam" sty>Boylam (Y) </label>
-                <input style={{ width:"150px" }} onChange={event => onYChange(event)} id="boylam" required 
-                    onInvalid={e => e.target.setCustomValidity("Lütfen Boylam Giriniz !")}
-                    onInput={e => e.target.setCustomValidity('')}
-                    type="text"
-                    name="Y"
-                    pattern="/^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/"/>
-                <br/>
-                <input type="submit" value="Göster" style={{ float: "right", marginTop:"10px" }} id="goster"/>
+                    {/* errors.email.type === "required"    -    errors.email.type === "pattern" */}
+                    {errors.enlemX && <p className="sorgu-form-hata-label">Hatalı Enlem!</p> }
+                </div>
+                <div>
+                    <label className="sorgu-label" htmlFor="boylamY">Boylam (Y) </label>
+                    <input className={errors.boylamY ? 'sorgu-form-data-error' : 'sorgu-form-data-normal'} 
+                        value={undefined}
+                        type="text"
+                        id="boylamY"
+                        {...register("boylamY", {
+                            onChange: event => onYChange(event),
+                            required: true,
+                            pattern: LONGITUDE_REGEX
+                        })}
+                    />
+                    {/* errors.email.type === "required"    -    errors.email.type === "pattern" */}
+                    {errors.boylamY && <p className="sorgu-form-hata-label">Hatalı Boylam!</p> }
+                </div>
+                
+                <input type="submit" value="Göster" style={{ float: "right", marginTop:"10px", marginLeft:"10px" }} id="goster"/>
+                <input type="button" onClick={() => reset()} value="Temizle" style={{ float: "right", marginTop:"10px" }} id="clear"/>
             </form>
         </Box>
     );

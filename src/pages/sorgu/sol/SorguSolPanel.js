e42a01da918f35aabd5d420a3d64688975938e42
outputtxt <142933373+outputtxt@users.noreply.github.com>
2023-09-18 13:31:16 +0000

    Update 3 files

--- src/pages/sorgu/sol/SorguSolPanel.js
+++ src/pages/sorgu/sol/SorguSolPanel.js
@@ -6,50 +6,51 @@ import KoordinatSorguPanel from "./koordinat/KoordinatSorguPanel";
 import KestirmeSorguPanel from "./kestirme/KestirmeSorguPanel";
 import "./SorguSolPanel.css";
 
-
 const SorguSolPanel = () => {
-    const [active, setActive] = useState(SorguTipi[2].name);
+  const [active, setActive] = useState(SorguTipi[0].name);
 
-    const getSorguPanel = () => {
-      if(!active){
-        return (<p>Boş Sorgu Seçimi</p>);
-      }
-
-      switch(active){
-        case SorguTipi[0].name:
-          return <KonumSorguPanel />;
-        case SorguTipi[1].name:
-          return <BazSorguPanel />;
-        case SorguTipi[2].name:
-          return <KoordinatSorguPanel />;
-        case SorguTipi[3].name:
-          return <KestirmeSorguPanel />;
-        default:
-          return (<p> Your payment selection: {active} </p>);
-      }
+  const getSorguPanel = () => {
+    if (!active) {
+      return <p>Boş Sorgu Seçimi</p>;
     }
 
-    return (
-      <div style={{display: "flex", flexDirection:"column"}}>
-        <div className="sorgu-sol-tab-group">
-          {SorguTipi.map(item => (
-            <button className={active === item.name ? "sorgu-sol-tab sorgu-sol-tab-active" : "sorgu-sol-tab" } 
-              key={item.name}
-              onClick={() => setActive(item.name)}
-            >
-              {item.name}
-            </button>
-          ))}
-        </div>
-        <div className="sorgu-sol-content">
-          { getSorguPanel() }
-        </div>
+    switch (active) {
+      case SorguTipi[0].name:
+        return <KonumSorguPanel />;
+      case SorguTipi[1].name:
+        return <BazSorguPanel />;
+      case SorguTipi[2].name:
+        return <KoordinatSorguPanel />;
+      case SorguTipi[3].name:
+        return <KestirmeSorguPanel />;
+      default:
+        return <p> Your payment selection: {active} </p>;
+    }
+  };
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div className="sorgu-sol-tab-group">
+        {SorguTipi.map((item) => (
+          <button
+            className={
+              active === item.name
+                ? "sorgu-sol-tab sorgu-sol-tab-active"
+                : "sorgu-sol-tab"
+            }
+            key={item.name}
+            onClick={() => setActive(item.name)}
+          >
+            {item.name}
+          </button>
+        ))}
       </div>
-      );
-}
+      <div className="sorgu-sol-content">{getSorguPanel()}</div>
+    </div>
+  );
+};
 
 export default SorguSolPanel;
-
 
 //// SAMPLE STYLED button
 // import styled from "styled-components";
--- src/pages/sorgu/sol/konum/KonumSorguPanel.js
+++ src/pages/sorgu/sol/konum/KonumSorguPanel.js
@@ -1,11 +1,50 @@
+import { useState } from "react";
+import Box from "@mui/material/Box";
+
+export const KonumSorguTipi = [
+  {
+    id: 0,
+    name: "Son Konum",
+  },
+  {
+    id: 1,
+    name: "Geçmiş",
+  },
+  {
+    id: 2,
+    name: "Son Baz",
+  },
+  {
+    id: 3,
+    name: "Son 1-7 Gün",
+  },
+];
 
 const KonumSorguPanel = () => {
- 
-    return (
-        <div>
-            KONUM SORGU PANEL
-        </div>
-    );
+  const [active, setActive] = useState(KonumSorguTipi[0].name);
+
+  
+
+  return (
+    <div>
+      {/* <div className="sorgu-fieldset"> */}
+      <Box
+        component="fieldset"
+        className="sorgu-fieldset"
+        style={{ display: "flex", flexDirection: "column" }}
+        onChange={(event) => setActive(event.target.value)}
+      >
+        {KonumSorguTipi.map((item) => (
+          <div>
+            <label className="sorgu-label" htmlFor="sonKonum">
+              {item.name}
+            </label>
+            <input type="radio" value={item.id} name="konumSorguTipi" />
+          </div>
+        ))}
+      </Box>
+    </div>
+  );
 };
 
 export default KonumSorguPanel;

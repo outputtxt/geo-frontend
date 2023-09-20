974b78b1a39c34436147a9b8a653f9e4cb2eed06
outputtxt <142933373+outputtxt@users.noreply.github.com>
2023-09-20 13:27:08 +0000

    Update KonumSorguPanel.js

--- src/pages/sorgu/sol/konum/KonumSorguPanel.js
+++ src/pages/sorgu/sol/konum/KonumSorguPanel.js
@@ -21,9 +21,15 @@ export const KonumSorguTipi = [
 ];
 
 const KonumSorguPanel = () => {
-  const [active, setActive] = useState(KonumSorguTipi[0].name);
+  const [active, setActive] = useState(KonumSorguTipi[0].id);
+  //   const [sonGun, setSonGun] =
 
-  
+  const onActiveChange = (event) => {
+    // alert(event.target.value);
+    // console.log(active + " : " + event.target.value);
+    setActive(event.target.value);
+    // console.log(active + " : " + event.target.value);
+  };
 
   return (
     <div>
@@ -32,16 +38,41 @@ const KonumSorguPanel = () => {
         component="fieldset"
         className="sorgu-fieldset"
         style={{ display: "flex", flexDirection: "column" }}
-        onChange={(event) => setActive(event.target.value)}
       >
-        {KonumSorguTipi.map((item) => (
-          <div>
-            <label className="sorgu-label" htmlFor="sonKonum">
-              {item.name}
-            </label>
-            <input type="radio" value={item.id} name="konumSorguTipi" />
-          </div>
-        ))}
+        <div onChange={(event) => onActiveChange(event)}>
+          {KonumSorguTipi.map((item) => (
+            <div>
+              <label className="sorgu-label" htmlFor={item.id}>
+                {item.name}
+              </label>
+              <input
+                type="radio"
+                value={item.id}
+                key={item.id}
+                id={item.id}
+                name="konumSorguTipi"
+              />
+              {active == 1 ? (
+                <select
+                  value="Avea"
+                  style={{
+                    marginRight: "100px",
+                    width: "150px",
+                    float: "right",
+                  }}
+                  id="operatorSelect"
+                  required
+                >
+                  <option value="Avea">Avea</option>
+                  <option value="Turkcell">Turkcell</option>
+                  <option value="Vodafone">Vodafone</option>
+                </select>
+              ) : (
+                <br />
+              )}
+            </div>
+          ))}
+        </div>
       </Box>
     </div>
   );

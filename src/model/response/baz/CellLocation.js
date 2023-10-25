bf0d2c4020d47cf721979caeb6525d46f14b5b2b
outputtxt <142933373+outputtxt@users.noreply.github.com>
2023-10-25 13:29:03 +0000

    Update 2 files

---
+++ src/model/response/baz/CellLocation.js
@@ -0,0 +1,12 @@
+export default class CellLocation {
+  constructor(X, Y, angle, adres) {
+    this.X = parseFloat(X); // center X coordinate
+    this.Y = parseFloat(Y); // center Y coordinate
+    // angle of the cell, if 0 than its a full circle,
+    // if angle exist, draw default sector, see LeafletConstants,
+    // default angle limit is +-32, and default radius is 100m
+    this.angle = parseFloat(angle);
+
+    this.adres = adres;
+  }
+}
--- src/service/rest/BazSorguService.js
+++ src/service/rest/BazSorguService.js
@@ -1,4 +1,6 @@
 import CellSorguResponse from "../../model/response/baz/CellSorguResponse";
+import CellLocation from "../../model/response/baz/CellLocation";
+import OperatorTipi from "../../model/enum/OperatorTipi";
 
 export default class bazSorguService {
   static cellSorgula(operator, cellId) {
@@ -8,8 +10,62 @@ export default class bazSorguService {
       2120514563,
       39.90845,
       32.75165,
-      3,
+      340,
       "GAZI Istanbul Yolu",
     );
+  }
+
+  static bazListeSorgula(operator) {
+    var cellList = [];
+
+    if (operator === OperatorTipi[0]) {
+      // AVEA
+      cellList.push(new CellLocation(39.915316, 32.758462, 60, "adres"));
+      cellList.push(new CellLocation(39.915316, 32.758462, 160, "adres"));
+      cellList.push(new CellLocation(39.915316, 32.758462, 230, "adres"));
+
+      cellList.push(new CellLocation(39.909786, 32.757411, 355, "adres"));
+      cellList.push(new CellLocation(39.909786, 32.757411, 85, "adres"));
+      cellList.push(new CellLocation(39.909786, 32.757411, 275, "adres"));
+
+      cellList.push(new CellLocation(39.909786, 32.757218, 30, "adres"));
+      cellList.push(new CellLocation(39.909786, 32.757218, 40, "adres"));
+      cellList.push(new CellLocation(39.909786, 32.757218, 100, "adres"));
+      cellList.push(new CellLocation(39.909786, 32.757218, 110, "adres"));
+      cellList.push(new CellLocation(39.909786, 32.757218, 230, "adres"));
+
+      cellList.push(new CellLocation(39.908847, 32.761209, 355, "adres"));
+      cellList.push(new CellLocation(39.908847, 32.761209, 5, "adres"));
+
+      cellList.push(new CellLocation(39.91383, 32.767861, 20, "adres"));
+      cellList.push(new CellLocation(39.91383, 32.767861, 110, "adres"));
+      cellList.push(new CellLocation(39.91383, 32.767861, 220, "adres"));
+      cellList.push(new CellLocation(39.91383, 32.767861, 230, "adres"));
+
+      cellList.push(new CellLocation(39.910469, 32.768247, 60, "adres"));
+      cellList.push(new CellLocation(39.910469, 32.768247, 270, "adres"));
+
+      cellList.push(new CellLocation(39.910485, 32.767206, 1, "adres"));
+      cellList.push(new CellLocation(39.910485, 32.767206, 120, "adres"));
+      cellList.push(new CellLocation(39.910485, 32.767206, 240, "adres"));
+
+      cellList.push(new CellLocation(39.90867, 32.76557, 340, "adres"));
+      cellList.push(new CellLocation(39.90867, 32.76557, 350, "adres"));
+      cellList.push(new CellLocation(39.90867, 32.76557, 80, "adres"));
+      cellList.push(new CellLocation(39.90867, 32.76557, 60, "adres"));
+      cellList.push(new CellLocation(39.90867, 32.76557, 180, "adres"));
+      cellList.push(new CellLocation(39.90867, 32.76557, 225, "adres"));
+      cellList.push(new CellLocation(39.90867, 32.76557, 270, "adres"));
+
+      cellList.push(new CellLocation(39.913678, 32.773236, 300, "adres"));
+      cellList.push(new CellLocation(39.913678, 32.773236, 90, "adres"));
+      cellList.push(new CellLocation(39.913678, 32.773236, 190, "adres"));
+    } else if (operator === OperatorTipi[1]) {
+      // TURKCELL
+    } else {
+      // if (operator === OperatorTipi[2]) { // VODAFONE
+    }
+
+    return cellList;
   }
 }

eb83b455e10be6c4b12f18a729fcd1f29425f19e
outputtxt <142933373+outputtxt@users.noreply.github.com>
2024-01-08 13:23:22 +0000

    Update Profile.js

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,7 @@
 import { authInfoStore, visibilityStore } from "../util/CoreStore";
 import { useSnapshot } from "valtio";
 import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+import Box from "@mui/material/Box";
 
 const Profile = () => {
   const { username, role } = useSnapshot(authInfoStore);
@@ -10,28 +11,37 @@ const Profile = () => {
   };
 
   return (
-    <div className="container">
-      <header className="jumbotron" style={{ marginTop: "100px" }}>
-        <h1>
-          <strong>{username}</strong>
-        </h1>
-        <h6>{role}</h6>
-      </header>
-      <p>
-        <button
-          className="logoutBtn"
-          onClick={changePassword}
-          title="Şifre Değiştir"
-          style={{
-            background: "transparent",
-            border: "none",
-            color: "white",
-            fontSize: "100px",
-          }}
-        >
-          <ManageAccountsIcon fontSize="inherit" />
-        </button>
-      </p>
+    <div className="container center-flex">
+      <Box
+        component="fieldset"
+        className="sorgu-fieldset-min"
+        style={{ minWidth: "500px" }}
+      >
+        <header className="jumbotron" style={{ marginTop: "100px" }}>
+          <h1>
+            <strong>{username}</strong>
+          </h1>
+          <h6>{"Rol: " + role.substring(5).replaceAll("_", " ")}</h6>
+        </header>
+        <p>
+          <button
+            className="logoutBtn"
+            onClick={changePassword}
+            title="Şifre Değiştir"
+            style={{
+              background: "transparent",
+              border: "none",
+              color: "white",
+              fontSize: "100px",
+            }}
+          >
+            <ManageAccountsIcon
+              fontSize="inherit"
+              sx={{ color: "var(--background-color-2)" }}
+            />
+          </button>
+        </p>
+      </Box>
     </div>
   );
 };
